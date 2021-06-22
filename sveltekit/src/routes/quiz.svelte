586e784b26ba2299b8fc80b203337c5af2e07ab5
Christian Pfisterer <ch.pfisterer@gmail.com>
2021-06-22 18:46:42 +0000

    finished sveltekit migration

--- sveltekit/src/routes/quiz.svelte
+++ sveltekit/src/routes/quiz.svelte
@@ -1,14 +1,38 @@
+<script context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch, session, context }) {
+		console.log(`Query: ${page.query}`)
+
+		const roundsParam = page.query.get('rounds');
+		const questionsParam = page.query.get('questions')
+		const res = await fetch(`/api/${roundsParam}/${questionsParam}`);
+
+		if (res.ok) return { props: { quiz: await res.json() } };
+
+		return {
+			status: res.status,
+			error: new Error(`Could not load /api`)
+		};
+	}
+</script>
+
 <script>
 	import SmallHeader from '$lib/SmallHeader.svelte';
 	import Step from '$lib/Step.svelte';
 	import Question from '$lib/Question.svelte';
 	import Button from '$lib/Button.svelte';
 	import { numberOfQuestions, numberOfRounds } from '../stores/inputStore';
+	import QuizRound from '$lib/QuizRound.svelte';
+
+	export let quiz = {};
+	export const rounds = 0;
 
 	const question = 'Hier ist dein Musikquiz';
 	const buttonText = 'Download';
-    const QUESTIONDURATION = 2;
-    const minutes = $numberOfQuestions * QUESTIONDURATION * $numberOfRounds
+	const QUESTIONDURATION = 2;
+	const minutes = $numberOfQuestions * QUESTIONDURATION * $numberOfRounds;
 </script>
 
 <div class="quiz">
@@ -16,34 +40,36 @@
 	<div class="quiz-color-text">
 		<Step stepNumber={3} />
 		<Question {question} />
-		<div class="mm-quiz-summary">
+		<div class="quiz-summary">
 			<div>
-				<span class="mm-quiz-summary-number">{$numberOfQuestions}</span><span
-					class="mm-quiz-round-container-header">Fragen</span
+				<span class="quiz-summary-number">{$numberOfQuestions}</span><span
+					class="quiz-round-container-header">Fragen</span
 				>
 			</div>
 			<div class="quiz-color">
-				<span class="mm-quiz-summary-number">{$numberOfRounds}</span><span
-					class="mm-quiz-round-container-header">Runden</span
+				<span class="quiz-summary-number">{$numberOfRounds}</span><span
+					class="quiz-round-container-header">Runden</span
 				>
 			</div>
 			<div class="quiz-color">
-				<span class="mm-quiz-summary-number">~{minutes}</span><span class="mm-quiz-round-container-header"
+				<span class="quiz-summary-number">~{minutes}</span><span class="quiz-round-container-header"
 					>Minuten</span
 				>
 			</div>
 		</div>
-		<div id="quizGrid" class="mm-quiz-container">
-			<!-- The quiz result will be added here -->
+		<div id="quizGrid" class="quiz-container">
+			{#each quiz.musicQuiz.rounds as quizRound}
+				<QuizRound round={quizRound.roundNumber} questions={quizRound.questions} />
+			{/each}
 		</div>
 		<Button {buttonText} />
 	</div>
 </div>
 
 <style>
-    :global(html) {
-        background-color: #f4a261;
-    }
+	:global(html) {
+		background-color: #f4a261;
+	}
 
 	.quiz {
 		overflow-x: hidden;
@@ -54,31 +80,27 @@
 		color: #264653;
 	}
 
-	.mm-quiz-summary {
+	.quiz-summary {
 		display: grid;
 		justify-content: space-evenly;
 		margin-top: 10px;
 		border: 4px solid #e9c46a;
 	}
 
-	.mm-quiz-summary-number {
+	.quiz-summary-number {
 		color: #e9c46a;
 		font-size: larger;
 		font-weight: 700;
 		padding: 0 10px 0 10px;
 	}
 
-	.mm-quiz-round-container-header {
+	.quiz-round-container-header {
 		font-size: large;
 		font-family: OpenSans-Bold;
 		text-align: center;
 	}
 
-	.mm-quiz-summary-text {
-		font-size: large;
-	}
-
-	.mm-quiz-container {
+	.quiz-container {
 		display: grid;
 		/* grid-template-columns: 50% 50%; */
 		justify-content: space-evenly;
@@ -86,19 +108,19 @@
 	}
 
 	@media only screen and (min-width: 1280px) {
-		.mm-quiz-summary {
+		.quiz-summary {
 			grid-template-columns: 30% 30% 40%;
 		}
 
-		.mm-quiz-summary-number {
+		.quiz-summary-number {
 			font-size: xx-large;
 		}
 
-		.mm-quiz-round-container-header {
+		.quiz-round-container-header {
 			font-size: x-large;
 		}
 
-		.mm-quiz-container {
+		.quiz-container {
 			grid-template-columns: 50% 50%;
 		}
 	}
