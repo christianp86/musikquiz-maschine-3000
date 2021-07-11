a3a649f70c8716e9ae9e0a542d2dc3d7f73e5872
Christian Pfisterer <fidsch@icloud.com>
2021-07-11 12:29:42 +0000

    finished start page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Header from '$lib/Header.svelte';
+	import Highlights from '$lib/Highlights.svelte';
 	import MainText from '$lib/Maintext.svelte';
 	import NavButton from '$lib/NavButton.svelte';
 
+	const highlights =
+		'Für Musikquiz-Hosts · für deine Musikquiz-Bedürfnisse · für denkwürdige Quiz-Abende';
 	const introText =
 		'Es ist mega einfach. Lege fest wie viele Fragen du pro Runde spielen willst und wie viele Runden das Quiz haben soll. Die phänomenale Musikquiz Maschine 3000 wählt danach für dich zufällig die Quizmaster-Fragen aus uuuuund … Zack, fertig – dein Musikquiz!';
 	const buttonText = 'Erstellen! Jetzt!';
@@ -13,8 +16,9 @@
 </svelte:head>
 
 <Header />
+<Highlights {highlights} />
 <MainText mainText={introText} />
-<NavButton {buttonText} link="questions"/>
+<NavButton {buttonText} link="questions" />
 
 <style>
 	:global(*) {
@@ -23,6 +27,7 @@
 		padding: 0;
 		font-family: 'Open Sans';
 	}
+
 	:global(body) {
 		background-color: #264653ff;
 	}
