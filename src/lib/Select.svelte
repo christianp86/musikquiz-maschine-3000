f782b4d2b98b5299627a2436bb6cd6e0f57c4b9a
Christian Pfisterer <ch.pfisterer@gmail.com>
2021-07-30 16:06:29 +0000

    started implementing using svelte-select

---
+++ src/lib/Select.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Select from 'svelte-select@4.2.7';
+
+	const items = ['One', 'Two', 'Three'];
+
+	const groupBy = (item) => item.group;
+	const optionIdentifier = 'id';
+	const getOptionLabel = (option) => option.name;
+	const getSelectionLabel = (option) => option.name;
+</script>
+
+<h2>Async</h2>
+<Select {loadOptions} {optionIdentifier} {getSelectionLabel} {getOptionLabel} {Item} placeholder="Search for 🍺"></Select>
