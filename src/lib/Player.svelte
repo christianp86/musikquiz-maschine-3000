fe7a9e24afce4d5a997ff1f8a852debbc95b226a
Christian Pfisterer <ch.pfisterer@gmail.com>
2021-11-01 21:22:03 +0000

    added players page

---
+++ src/lib/Player.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import type { Player } from '$lib/utils/quiz_interfaces';
+	import { players } from '../stores/player';
+    import { numberOfPlayers } from '../stores/inputStore'; 
+	let newPlayer = '';
+	let allPlayers: Player[] = [];
+
+	/**
+	 * Add a new player to the player store
+	 */
+	function addPlayer() {
+		console.log(`New player: ${newPlayer}`);
+		const player: Player = { name: newPlayer, correctQuestions: [] };
+		allPlayers = [...allPlayers, player];
+		players.set(allPlayers);
+		newPlayer = '';
+        numberOfPlayers.update(n => n + 1)
+	}
+</script>
+
+<div>
+	<input
+		type="text"
+		bind:value={newPlayer}
+		required
+		class="input-questions input-text questions"
+		id="newplayer"
+		min="3"
+		max="20"
+	/>
+	<button class="button" on:click="{addPlayer}">+</button>
+    <p>{$numberOfPlayers} Spieler/Teams.</p>
+</div>
+
+<style>
+	div {
+		display: flex;
+		flex-direction: row;
+	}
+	.questions {
+		background: #2a9d8f;
+	}
+
+	.input-questions {
+		border: none;
+		border-bottom: 4px solid #043b35;
+	}
+
+	.input-text {
+		font-size: xx-large;
+		text-align: center;
+		color: #e9c46a;
+		width: 10vw;
+	}
+
+    .button {
+		width: max-content;
+		border: 1px solid #043b35;
+		border-radius: 4px;
+		font-family: OpenSans-Bold;
+        background: inherit;
+		color: #e9c46a;
+		text-align: center;
+		transition-duration: 0.4s;
+		cursor: pointer;
+	}
+
+</style>
