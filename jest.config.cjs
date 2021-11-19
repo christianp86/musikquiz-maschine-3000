1e41da96711672e99cb0ed517bbd721494ef43da
Christian Pfisterer <ch.pfisterer@gmail.com>
2021-11-19 14:33:19 +0000

    initial jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  // testEnvironment: 'jsdom',
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   transform: {
     "^.+\\.svelte$": [
