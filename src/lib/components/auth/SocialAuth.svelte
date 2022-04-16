88d8393fe0c587d94c2fe10c49124ef00b61c80d
Christian Pfisterer <ch.pfisterer@gmail.com>
2022-04-16 11:55:49 +0000

    access to private playlists via oauth scope

--- src/lib/components/auth/SocialAuth.svelte
+++ src/lib/components/auth/SocialAuth.svelte
@@ -1,37 +1,41 @@
 <script lang="ts">
-  import type { SupabaseClient } from '@supabase/supabase-js'
-  import Button from './Button.svelte'
-  import Text from './Text.svelte'
+  import type { SupabaseClient } from "@supabase/supabase-js";
+  import Button from "./Button.svelte";
+  import Text from "./Text.svelte";
 
-  export let supabaseClient : SupabaseClient
-  export let providers
-  export let socialLayout
-  export let socialButtonSize
-  export let socialColors
-  export let view
+  export let supabaseClient: SupabaseClient;
+  export let providers;
+  export let socialLayout;
+  export let socialButtonSize;
+  export let socialColors;
+  export let view;
 
-  let loading = false, error = ''
+  let loading = false,
+    error = "";
 
   const buttonStyles = {
     spotify: {
-      'background-color': '#000000',
-      color: 'white'
+      "background-color": "#000000",
+      color: "white",
     },
     apple: {
-      'background-color': '#ffffff',
-      color: 'black'
-    }
-  }
+      "background-color": "#ffffff",
+      color: "black",
+    },
+  };
 
-  $: hasProviders = providers && providers.length > 0
+  $: hasProviders = providers && providers.length > 0;
 
   async function handleProviderSignIn(provider) {
-    loading = true
+    loading = true;
 
-    const { error: signInError } = await supabaseClient.auth.signIn({ provider })
-    if (signInError) error = signInError.message
+    const { error: signInError } = await supabaseClient.auth.signIn(
+      { provider },
+      { scopes: "playlist-read-private" }
+    );
+    if (signInError) error = signInError.message;
 
-    loading = false
+    loading = false;
   }
 </script>
 
@@ -39,10 +43,18 @@
   <h2>Welcome</h2>
   <span class="heading">Sign in to Musikquiz Maschine 3000.</span>
 
-  <div class="providers" class:horizontal={socialLayout == 'horizontal'}>
+  <div class="providers" class:horizontal={socialLayout == "horizontal"}>
     {#each providers as provider}
-      <Button block icon={provider} size={socialButtonSize} style={socialColors ? buttonStyles[provider] : {}} on:click={() => handleProviderSignIn(provider)}>
-        {#if socialLayout == 'vertical'}{view == 'sign_up' ? 'Sign up' : 'Sign in'} with {provider}{/if}
+      <Button
+        block
+        icon={provider}
+        size={socialButtonSize}
+        style={socialColors ? buttonStyles[provider] : {}}
+        on:click={() => handleProviderSignIn(provider)}
+      >
+        {#if socialLayout == "vertical"}{view == "sign_up"
+            ? "Sign up"
+            : "Sign in"} with {provider}{/if}
       </Button>
     {/each}
   </div>
@@ -53,8 +65,7 @@
 {/if}
 
 <style>
-
-  *{
+  * {
     text-align: center;
   }
   .providers {
@@ -73,5 +84,4 @@
     line-height: 1.25rem;
     margin: 0 0 0.5rem 0;
   }
-
 </style>
