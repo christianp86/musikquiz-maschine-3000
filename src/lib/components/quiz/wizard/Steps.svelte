96041389e123b57502fd2dfb615ddeca376d9828
Christian Pfisterer <ch.pfisterer@gmail.com>
2022-05-02 18:19:20 +0000

    tailwind color customization added, wizard component added, create route added

---
+++ src/lib/components/quiz/wizard/Steps.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  interface Step {
+    number: number;
+    label: string;
+  }
+  const steps: Step[] = [
+    { number: 1, label: "Einstellungen" },
+    { number: 2, label: "Playlist" },
+    { number: 3, label: "Fragen" },
+    { number: 4, label: "Ergebnis" },
+  ];
+</script>
+
+<div>
+  <h2 class="sr-only">Steps</h2>
+
+  <div
+    class="relative after:inset-x-0 after:h-0.5 after:absolute after:top-1/2 after:-translate-y-1/2 after:block after:rounded-lg after:bg-primary-75"
+  >
+    <ol
+      class="relative z-10 flex justify-between text-sm font-medium text-gray-500"
+    >
+      {#each steps as step}
+        <li class="flex items-center p-2">
+          <span
+            class="w-6 h-6 text-[10px] font-bold leading-6 bg-primary-75 text-center rounded-full"
+          >
+            {step.number}
+          </span>
+          <span class="hidden sm:block sm:ml-2"> {step.label} </span>
+        </li>
+      {/each}
+    </ol>
+  </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,38 @@
+function getCSSVariable(variable) {
+  return `var(${variable})`;
+}
+
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      base_color: {
+        10: getCSSVariable('--clr-10'),
+        20: getCSSVariable('--clr-20'),
+        30: getCSSVariable('--clr-30'),
+        40: getCSSVariable('--clr-40'),
+        50: getCSSVariable('--clr-50'),
+        60: getCSSVariable('--clr-60'),
+        70: getCSSVariable('--clr-70'),
+        80: getCSSVariable('--clr-80'),
+        90: getCSSVariable('--clr-90'),
+        95: getCSSVariable('--clr-95'),
+      },
+      primary: {
+        10: getCSSVariable('--primary-10'),
+        15: getCSSVariable('--primary-15'),
+        25: getCSSVariable('--primary-25'),
+        50: getCSSVariable('--primary-50'),
+        75: getCSSVariable('--primary-75'),
+      },
+      accent: {
+        10: getCSSVariable('--accent-10'),
+        15: getCSSVariable('--accent-15'),
+        25: getCSSVariable('--accent-25'),
+        50: getCSSVariable('--accent-50'),
+        75: getCSSVariable('--accent-75'),
+      }
+    },
     extend: {},
   },
   plugins: [],
