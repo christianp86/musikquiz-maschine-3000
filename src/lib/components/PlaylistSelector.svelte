47bfe0e7777d391ffd85bd0b26647b8d01e57f01
Christian Pfisterer <ch.pfisterer@gmail.com>
2022-06-30 20:49:36 +0000

    upgraded packages

--- src/lib/components/PlaylistSelector.svelte
+++ src/lib/components/PlaylistSelector.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
   import type { PlaylistBaseObject } from "$lib/utils/spotify/Types";
   export let playLists: PlaylistBaseObject[] = [];
+
+  const onChange = (event) => {
+    console.log(event.target.value);
+  }
 </script>
 
-<select name="Spotify Playlist" class="form-select form-select-sm bg-base_color-10 border-2 border-accent-75 max-w-min">
+<select on:change={onChange} name="Spotify Playlist" class="form-select form-select-sm bg-base_color-10 border-2 border-accent-75 max-w-min">
   {#each playLists as playlist}
     <option value={playlist.id}>{playlist.name}</option>
   {/each}
