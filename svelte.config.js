627eda3272ef98df4921db99c8263cb2b90763bb
Christian Pfisterer <ch.pfisterer@gmail.com>
2022-09-26 09:59:16 +0000

    Upgrade to sveltekit .405

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-import netlify from '@sveltejs/adapter-netlify'
-import { resolve } from "path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -9,15 +8,7 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: netlify(),
-		vite: {
-			resolve: {
-				alias: {
-					$routes: resolve("./src/routes")
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
